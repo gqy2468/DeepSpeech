81e5c2f50b16f14ba39c0abe011225dae01c8008
gqy2468 <gqy2468@163.com>
2019-08-21 11:31:48 +0800

    Update check-thchs30.sh

--- bin/check-thchs30.sh
+++ bin/check-thchs30.sh
@@ -26,16 +26,26 @@ IFS="
 if [ "$2" = "Y" ];then
     mkdir "/tmp/copy"
 fi
+if [ -f "/tmp/${1##*/}" ];then
+    rm "/tmp/${1##*/}"
+    echo "tmp file exist and delete!"
+fi
 i=1
 for line in `cat $1`
 do
     #echo line $i:
     fpath=$(echo $line | awk -F"," '{print $1}')
     fsize=$(echo $line | awk -F"," '{print $2}')
+    cword=$(echo $line | awk -F"," '{print $3}')
     fsize2=$(ls -l $fpath | awk '{print $5}')
     fname=${fpath##*/}
     if [ "$fsize" != "$fsize2" ];then
         echo line $i:$fsize"[ != ]"$fsize2
+    fi
+    if [ "$i" = "1" ];then
+	echo "$line" >> "/tmp/${1##*/}"
+    else
+	echo "$fpath,$fsize2,$cword" >> "/tmp/${1##*/}"
     fi
     if [ "$2" = "Y" ];then
 	ffmpeg -i "$fpath" -c:a copy "/tmp/copy/$fname"
